1d0d8f186ab001a26a7124d15312237410c5283b
Yaroslav Grebnov <grebnov@gmail.com>
2025-03-28 13:14:43 +0100

    Update dispatching

--- pool/fixed.go
+++ pool/fixed.go
@@ -2,16 +2,14 @@ package pool
 
 type fixed struct {
 	available chan interface{}
-	all       chan interface{}
-	buf       chan interface{}
+	len       chan struct{}
 	newFn     func() interface{}
 }
 
 func NewFixed(capacity uint, newFn func() interface{}) Pool {
 	return &fixed{
 		available: make(chan interface{}, capacity),
-		all:       make(chan interface{}, capacity),
-		buf:       make(chan interface{}, 1024),
+		len:       make(chan struct{}, capacity),
 		newFn:     newFn,
 	}
 }
@@ -21,32 +19,11 @@ func (p *fixed) Get() interface{} {
 	case el := <-p.available:
 		return el
 
-	case el := <-p.buf:
-		return el
-
-	default:
-		var el interface{}
-
-		if len(p.all) < cap(p.all) {
-			el = p.newFn()
-		} else {
-			el = <-p.all
-		}
-
-		select {
-		case p.all <- el:
-		case p.buf <- el:
-		default:
-		}
-		return el
+	case p.len <- struct{}{}:
+		return p.newFn()
 	}
 }
 
 func (p *fixed) Put(el interface{}) {
-	select {
-	case p.available <- el:
-	case p.all <- el:
-	case p.buf <- el:
-	default:
-	}
+	p.available <- el
 }
--- tests/benchmark_test.go
+++ tests/benchmark_test.go
@@ -2,6 +2,7 @@ package tests
 
 import (
 	"context"
+	"runtime"
 	"testing"
 
 	"github.com/ygrebnov/workers"
@@ -14,11 +15,11 @@ func BenchmarkWorkers(b *testing.B) {
 		tasks      []interface{}
 	}{
 		// less big tasks
-		{"fixed4_n8_size10_wait50", 4, tasksN8Size10},
+		{"fixed4_n8_size10_wait50", uint(runtime.NumCPU()), tasksN8Size10},
 		{"dynamic_n8_size10_wait50", 0, tasksN8Size10},
 
 		// more small tasks
-		{"fixed4_n256_size2_wait50", 4, tasksN256Size2},
+		{"fixed4_n256_size2_wait50", uint(runtime.NumCPU()), tasksN256Size2},
 		{"dynamic_n256_size2_wait50", 0, tasksN256Size2},
 	}
 	for _, test := range tests {
