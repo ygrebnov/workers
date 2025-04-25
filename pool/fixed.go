bbb89b5ee8b87f77b37babea175191206b546d1d
Yaroslav Grebnov <grebnov@gmail.com>
2025-04-25 23:20:02 +0200

    Add docstrings

--- pool/fixed.go
+++ pool/fixed.go
@@ -6,6 +6,9 @@ type fixed struct {
 	newFn     func() interface{}
 }
 
+// NewFixed creates a new fixed-size pool of workers with the given capacity and a function to create new workers.
+// The pool size is limited to the specified capacity, and if all workers are in use,
+// it blocks until one is returned to the pool and becomes available.
 func NewFixed(capacity uint, newFn func() interface{}) Pool {
 	return &fixed{
 		available: make(chan interface{}, capacity),
--- pool/pool.go
+++ pool/pool.go
@@ -1,6 +1,10 @@
 package pool
 
+// Pool is an interface that defines methods on a pool of workers.
 type Pool interface {
+	// Get returns a worker from the pool.
 	Get() interface{}
+
+	// Put returns a worker back to the pool.
 	Put(interface{})
 }
